d8891642d1a2401069037dda5d9ff2f77cbb4848
ayomide <adebayoayomide396@gmail.com>
2022-11-07 20:13:17 +0100

    first commit

---
+++ js/nav.js
@@ -0,0 +1,46 @@
+// const hamburger = document.querySelector('.hamburger');
+// const navLinks = document.querySelector('.nav-links');
+// const links = document.querySelectorAll('.nav-links li');
+// // const link = document.querySelector('.nav-link');
+
+ 
+
+
+
+// // hamburger.addEventListener('click', () => {
+// //     navLinks.classList.toggle('open')
+// //     links.forEach(link => {
+// //         link.classList.toggle('fade')
+// //     });
+// //   
+// // });
+
+
+
+
+
+// hamburger.addEventListener('click', ()=>{
+//     //Animate Links
+//      navLinks.classList.toggle("open");
+//      links.forEach(link => {
+//         //  link.classList.toggle("fade");
+//      });
+ 
+//      //Hamburger Animation
+//      hamburger.classList.toggle("toggle");
+// });
+
+// links.forEach(link => {
+//     link.addEventListener('click', () => {
+//         navLinks.classList.remove('open')
+//     hamburger.classList.toggle("toggle");
+
+//     })
+// });
+
+
+// // link.addEventListener('click', () => {
+// //     navLinks.classList.remove('open')    
+// //     hamburger.classList.toggle("toggle");
+    
+// // })
