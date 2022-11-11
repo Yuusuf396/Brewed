5eb282e3f8dd0145fe27944ea0e678efd982bb09
ayomide <adebayoayomide396@gmail.com>
2022-11-11 09:14:04 +0100

    shop page

---
+++ js/img.js
@@ -0,0 +1,21 @@
+// const mainImg = document.querySelector('.main_img');
+// const smallImg = document.querySelectorAll('.small_image');
+
+
+ 
+// smallImg.forEach(small => {
+//     small.addEventListener('click', () => {
+        // mainImg.src = small.src;
+//         console.log('Win');
+        
+//     })
+    
+// })
+const products = document.querySelectorA('.product_items')
+products.forEach(product => {
+    product.addEventListener('clicl', () => {
+        
+        window.location.href='sprod.html'
+    })
+    
+});
